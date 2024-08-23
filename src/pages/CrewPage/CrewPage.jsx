fd56c83ba00e0b5c009c3aff45ce128f6c4a5ce4
crapuscul <sabri0hamid@gmail.com>
2024-08-23 13:55:17 +0100

    crewpage

--- src/pages/CrewPage/CrewPage.jsx
+++ src/pages/CrewPage/CrewPage.jsx
@@ -1,12 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./CrewPage.css"
 
 import Navbar from "../../components/navbar/navbar";
 
 export default function CrewPage() {
+
+  const crewMembers = {
+    crew1: {
+     picture:"./src/assets/images/crew/image-douglas-hurley.png",
+     
+      title: "COMMANDER",
+      name: "DOUGLAS HURLEY",
+      description: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2."
+    },
+    crew2: {
+     picture:"./src/assets/images/crew/image-mark-shuttleworth.png",
+      title: "MISSION SPECIALIST",
+      name: "MARK SHUTTLEWORTH",
+      description: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist."
+    },
+    crew3: {
+     picture:"./src/assets/images/crew/image-victor-glover.png",
+      title: "PILOT",
+      name: "VICTOR GLOVER",
+      description: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18. He was a crew member of Expedition 64, and served as a station systems flight engineer."
+    },
+    crew4: {
+      picture:"./src/assets/images/crew/image-anousheh-ansari.png",
+       title: "FLIGHT ENGINEER",
+       name: "ANOUSHEH ANSARI",
+       description: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. "
+     }
+  };
+  const [selectedCrew, setSelectedCrew] = useState("crew1");
+   // to dynamically change background-image
+    // Add class to body to target in the css
+     // UseEffect to add class to body
+  useEffect(() => {
+    document.body.classList.add('crew-body');
+     // Cleanup on unmount when the user moves to another page
+     return() => {
+      document.body.classList.remove('crew-body');
+     };
+
+  },[]);
+    // Function to render the content of the selected section
+  const renderCrewContent = () => {
+    const crewMember = crewMembers[selectedCrew];
+    return(
+      <div>
+        
+        <h2 className="crew-member-title">{crewMember.title}</h2>
+        <h1 className="crew-memeber-name">{crewMember.name}</h1>
+        <p className="crew-member-bio">{crewMember.description}</p>
+      </div>
+      )
+  }
+  const renderCrewPicture =()=> {
+    const crewMember = crewMembers[selectedCrew];
+    return(
+      <div><img src={crewMember.picture}/></div>
+    )
+  }
     return (
       <div>
-        <h1>Crew Page</h1>
-        
+        <body className="crew-body">
+          <main className="main-section">
+          <div className='slogan'><span>02</span>MEET YOUR CREW</div>
+          </main>
+          <div className="crew-members-content">
+          <div className="box-right"> {renderCrewPicture()}</div>
+          <div className="box-left">
+           <div className="crew-content">
+           {renderCrewContent()}
+            </div> 
+            <div className="crew-members-pagination">
+                  <div className={`dot ${selectedCrew === "crew1" ? "selected" : ""}`}
+                  onClick={()=> setSelectedCrew("crew1")}></div>
+                  <div className={`dot ${selectedCrew === "crew2" ? "selected" :""}`}
+                  onClick={()=> setSelectedCrew("crew2")}></div>
+                  <div className={`dot ${selectedCrew === "crew3" ? "selected" :""}`}
+                  onClick={()=> setSelectedCrew("crew3")}></div>
+                  <div className={`dot ${selectedCrew === "crew4" ? "selected" :""}`}
+                  onClick={()=> setSelectedCrew("crew4")}></div>
+              </div>
+
+            </div>
+           </div>
+        </body>
+
+                
       </div>
     );
   }
