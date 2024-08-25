56208146dfd46fc81103cbf93bc7785d216c9231
Hamid <sabri0hamid@gmail.com>
2024-08-25 21:09:49 +0100

    destination page re-imp => design sys

--- src/pages/DestinationPage/DestinationPage.jsx
+++ src/pages/DestinationPage/DestinationPage.jsx
@@ -10,7 +10,7 @@ export default function DestinationPage() {
             text: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites." ,
             distanceTitle: "AVG. DISTANCE",
             distance:"384,400 KM",
-            travelTimeTitle: "Est. travel time",
+            travelTimeTitle: "EST. TRAVEL TIME",
             travelTime:"3 DAYS",
             },
         section2: { 
@@ -19,7 +19,7 @@ export default function DestinationPage() {
                     text: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
                     distanceTitle: "AVG. DISTANCE",
                     distance:"225 MIL. KM",
-                    travelTimeTitle: "Est. travel time",
+                    travelTimeTitle: "EST. TRAVEL TIME",
                     travelTime:"9 MONTHS",
                 },
         section3: {  picture:"./src/assets/images/destination/image-europa.png",
@@ -28,7 +28,7 @@ export default function DestinationPage() {
                     text: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
                     distanceTitle: "AVG. DISTANCE",
                     distance:"628 MIL. KM",
-                    travelTimeTitle: "Est. travel time",
+                    travelTimeTitle: "EST. TRAVEL TIME",
                     travelTime:"3 YEARS",
          },
         section4: { picture:"./src/assets/images/destination/image-titan.png",
@@ -37,7 +37,7 @@ export default function DestinationPage() {
                     text: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn." ,
                     distanceTitle: "AVG. DISTANCE",
                     distance:"1.6 MIL. KM",
-                    travelTimeTitle: "Est. travel time",
+                    travelTimeTitle: "EST. TRAVEL TIME",
                     travelTime:"7 YEARS",
                     },
     };
@@ -91,7 +91,7 @@ export default function DestinationPage() {
     return (
         <> <div className='destination-body'>
              <main className='main-section'>
-             <div className='slogan'><span>01</span>PICK YOUR DESTINATION</div>
+             <div className='slogan-destination'><span>01</span>PICK YOUR DESTINATION</div>
                 
                 <div className='main-container'>
                 
