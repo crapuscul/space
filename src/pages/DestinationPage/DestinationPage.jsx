fd9c55ef0feb7bde60f4cfdd59a7f9fdb9db3431
Hamid <sabri0hamid@gmail.com>
2024-08-29 17:15:46 +0100

    ok

--- src/pages/DestinationPage/DestinationPage.jsx
+++ src/pages/DestinationPage/DestinationPage.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./DestinationPage.css";
+import moonImg from "../../assets/images/destination/image-moon.png";
+import marsImg from "../../assets/images/destination/image-mars.png";
+import europaImg from "../../assets/images/destination/image-europa.png";
+import titanImg from "../../assets/images/destination/image-titan.png";
 
 export default function DestinationPage() {
   // Define the sections with their respective elemets
   const sections = {
     section1: {
-      picture: require("./assets/images/destination/image-moon.png"),
+      picture: moonImg,
       heading: "MOON",
       text: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
       distanceTitle: "AVG. DISTANCE",
@@ -14,7 +18,7 @@ export default function DestinationPage() {
       travelTime: "3 DAYS",
     },
     section2: {
-      picture: "./src/assets/images/destination/image-mars.png",
+      picture: marsImg,
       heading: "MARS",
       text: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
       distanceTitle: "AVG. DISTANCE",
@@ -23,7 +27,7 @@ export default function DestinationPage() {
       travelTime: "9 MONTHS",
     },
     section3: {
-      picture: "./src/assets/images/destination/image-europa.png",
+      picture: europaImg,
 
       heading: "EUROPA",
       text: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
@@ -33,7 +37,7 @@ export default function DestinationPage() {
       travelTime: "3 YEARS",
     },
     section4: {
-      picture: "./src/assets/images/destination/image-titan.png",
+      picture: titanImg,
 
       heading: "TITAN",
       text: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
