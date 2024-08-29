27cda878a18660044413e6a0e1348177b62eb232
Hamid <sabri0hamid@gmail.com>
2024-08-29 20:35:49 +0100

    deprecated naming react naming error handlings

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage/HomePage'; 
-import DestinationPage from '../pages/DestinationPage/DestinationPage';
-import CrewPage from '../pages/CrewPage/CrewPage';
-import TechnologyPage from '../pages/TechnologyPage/TechnologyPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import DestinationPage from "../pages/DestinationPage/DestinationPage";
+import CrewPage from "../pages/CrewPage/CrewPage";
+import TechnologyPage from "../pages/TechnologyPage/TechnologyPage";
 
-
-
-export default function AppRoutes(){
-    return (
-        <>
-       
-            <Routes>
-                <Route  exact path="/" element={<HomePage/>}/>
-                <Route  path="/DestinationPage" element={<DestinationPage/>}/>
-                <Route  path="/CrewPage" element={<CrewPage/>}/>
-                <Route  path="/TechnologyPage" element={<TechnologyPage/>}/>
-            </Routes>
-        
-        </>
-    )
+export default function AppRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/DestinationPage" element={<DestinationPage />} />
+        <Route path="/CrewPage" element={<CrewPage />} />
+        <Route path="/TechnologyPage" element={<TechnologyPage />} />
+      </Routes>
+    </>
+  );
 }
